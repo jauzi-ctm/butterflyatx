5d14b492de3a7fd87ef24535a7055dadc20f8011
noobiest-Lord <127801056+noobiest-Lord@users.noreply.github.com>
2023-06-20 16:16:10 -0500

    ExploreEvents.js creation
    Created ExploreEvents.js; edited Form.js to include dual screen

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { ScrollView, StyleSheet, StatusBar } from 'react-native';
-import Form from "./src/components/Form";
-import { addEventFormFields } from "./src/utilities/formInfo";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ExploreEvents from './src/components/ExploreEvents.js';
+import  Form  from './src/components/Form.js'; // Update the import statement
+
+const Stack = createStackNavigator();
 
 const App = () => {
-	return (
-		<ScrollView style={styles.container}>
-			<Form title={"Add Event"} fields={addEventFormFields} submitText={"Add"} onSubmit={() => { console.log("add event"); }} />
-		</ScrollView>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ExploreEvents" component={ExploreEvents} />
+        <Stack.Screen name="Form" component={Form} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: StatusBar.currentHeight + 32
-	}
-});
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { View, Text, FlatList, Button } from "react-native";
+import { View, Text, FlatList, Button, StyleSheet, StatusBar, ScrollView } from "react-native";
 import InputField from "./InputField";
 import { formStyles } from "../styles/formStyles";
+import { addEventFormFields } from "../utilities/formInfo";
+
 
 const Form = props => {
     const { title, fields, submitText = "Submit", onSubmit } = props;
     const { container, titleText } = formStyles;
 
     return (
-        <View style={container}>
-            <Text style={titleText}>{title}</Text>
-            <FlatList data={fields} renderItem={({ item }) => <InputField label={item.label} placeholder={item.placeholder} />} />
-            <Button title={submitText} color={"gray"} onPress={onSubmit} />
+        <View style={styles.container}>
+        <Text style={titleText}>{"Add Event"}</Text>
+        <FlatList data={addEventFormFields} renderItem={({ item }) => <InputField label={item.label} placeholder={item.placeholder} />} />
+        <Button title={"Add"} color={"gray"} onPress={() => { console.log("add event"); } }></Button>
         </View>
-    );
+    )
+    
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight + 32
+	}
+});
 
 export default Form;
