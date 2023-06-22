51985146cd66e17814fb95a8df57cb75ad6ff4e3
noobiest-Lord <127801056+noobiest-Lord@users.noreply.github.com>
2023-06-22 12:01:07 -0500

    Updated UserInfo.js
    Not sure if it works. On console shows stored sucessfully but also shows undefined.

--- src/screens/UserInfo.js
+++ src/screens/UserInfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Text } from 'react-native';
 import { userFormFields } from '../utilities/formInfo';
 import Form from '../components/Form';
 import { formStyles } from '../styles/formStyles';
@@ -21,20 +22,40 @@ const UserInfo = () => {
     }
   };
 
+  const getUserData = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('userData');
+      if (userData !== null) {
+        const { firstName, lastName, age } = JSON.parse(userData);
+        console.log('User data:', firstName, lastName, age);
+      } else {
+        console.log('User data not found');
+      }
+    } catch (error) {
+      console.log('Error retrieving user data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Call the function to retrieve user data
+    getUserData();
+  }, []);
+
   return (
-    <Form
-      title={"Import your data"}
-      fields={userFormFields}
-      submitText={"Create"}
-      submitForm={() => {
-        useEffect(() => {
+    <>
+      <Form
+        title={"Import your data"}
+        fields={userFormFields}
+        submitText={"Create"}
+        submitForm={() => {
           // Call the function to store user data
-          storeUserData(formData);
-        }, []);
-      }}
-      formData={formData}
-      styles={formStyles} // Pass the styles object as a prop
-    />
+          storeUserData(formData.firstName, formData.lastName, formData.age);
+          {useEffect}
+        }}
+        formData={Object.entries(formData).map(([label, value]) => ({ label, value }))}
+        styles={formStyles} // Pass the styles object as a prop
+      />
+    </>
   );
 };
 
