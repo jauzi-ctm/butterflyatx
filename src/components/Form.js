5054032bdee318c679a5cd2f21bcc5efb4faeccc
Jeremy Moon <jeremy.moon16@gmail.com>
2023-07-06 15:02:40 -0500

    Fix form error

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { View, Text, FlatList, Alert, StyleSheet } from "react-native";
 import InputField from "./InputField";
-import { HamburgerButtonBack }  from "./HamburgerButtonBack";
+import { HamburgerButtonBack } from "./HamburgerButtonBack";
 import { useNavigation } from "@react-navigation/native";
 import ButtonUjval from "./ButtonUjval";
 
@@ -46,37 +46,40 @@ const Form = (props) => {
   };
 
   return (
-    <><HamburgerButtonBack /><View style={container}>
-      <Text style={titleText}>{title}</Text>
-      <Text style={instructionText}>* indicates required fields</Text>
-      <FlatList
-        style={{ flex: 9, paddingHorizontal: 16 }}
-        data={fields} // Render the input fields as a flat list
-        renderItem={({ item }) => {
-          if (item.type == "Button") {
-            return (
-              <View style={buttonContainer}>
-                <ButtonUjval data={{ label: submitText, whatAction: submitAction }} />
-                <ButtonUjval data={{ label: "Cancel", whatAction: navigation.goBack }} />
-              </View>
-            );
-          }
+    <>
+      <HamburgerButtonBack />
+      <View style={container}>
+        <Text style={titleText}>{title}</Text>
+        <Text style={instructionText}>* indicates required fields</Text>
+        <FlatList
+          style={{ flex: 9, paddingHorizontal: 16 }}
+          data={fields} // Render the input fields as a flat list
+          renderItem={({ item }) => {
+            if (item.type == "Button") {
+              return (
+                <View style={buttonContainer}>
+                  <ButtonUjval data={{ label: submitText, whatAction: submitAction }} />
+                  <ButtonUjval data={{ label: "Cancel", whatAction: navigation.goBack }} />
+                </View>
+              );
+            }
 
-          return (
-            <InputField
-              label={item.label}
-              type={item.type}
-              required={item.required}
-              multiline={item.multiline}
-              options={item.options}
-              placeholder={item.placeholder}
-              updateData={updateData}
-            />
-          );
-        }}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </View>
+            return (
+              <InputField
+                label={item.label}
+                type={item.type}
+                required={item.required}
+                multiline={item.multiline}
+                options={item.options}
+                placeholder={item.placeholder}
+                updateData={updateData}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    </>
   );
 };
 
--- src/components/HamburgerButtonBack.js
+++ src/components/HamburgerButtonBack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const HamburgerButtonBack = () => {
     const navigation = useNavigation();
@@ -10,9 +11,11 @@ export const HamburgerButtonBack = () => {
     };
 
     return (
-        <TouchableOpacity onPress={opengoBack}>
-            <Image source={require("../../assets/goBack.png")} style={styles.logoButton} />
-        </TouchableOpacity>
+        <SafeAreaView>
+            <TouchableOpacity onPress={opengoBack}>
+                <Image source={require("../../assets/goBack.png")} style={styles.logoButton} />
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
 
